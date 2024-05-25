b8435a04ebea568f495a2a4d4225071be9996284
Enes Yağmur <enesyagmur99@gmail.com>
2024-05-25 13:26:00 +0300

    header style ve mode butonu yapıyorum

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const changeModeFunc = () => {
   if (document.body.className) {
     document.body.classList.remove("light");
+    var button = document.getElementsByClassName("mode-button-right");
   } else {
     document.body.classList.add("light");
   }
