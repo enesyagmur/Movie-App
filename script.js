dcd6ba0bb78286c6a9d43c849d576c912cf2e6f5
Enes Yağmur <enesyagmur99@gmail.com>
2024-05-30 09:26:16 +0300

    gereksiz dosyalar silindi ve slider başa dönme özelliği düzeltildi

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ const changeModeFunc = () => {
   }
 };
 
-//SLIDER
+//SLIDER----------------------------------------------------------------
 let slider1 = document.querySelector("#first-slider");
 let slider2 = document.querySelector("#second-slider");
 let slider3 = document.querySelector("#thirt-slider");
 
 const scrollToRight = (slider) => {
   if (slider === "first-slider") {
-    if (slider1.scrollLeft === 1682) {
+    if (slider1.scrollLeft === 1664) {
       slider1.scrollBy({
         top: 0,
-        left: -1682,
+        left: -1664,
         behavior: "smooth",
       });
     } else {
@@ -32,10 +32,10 @@ const scrollToRight = (slider) => {
       });
     }
   } else if (slider === "second-slider") {
-    if (slider2.scrollLeft === 1682) {
+    if (slider2.scrollLeft === 1664) {
       slider2.scrollBy({
         top: 0,
-        left: -1682,
+        left: -1664,
         behavior: "smooth",
       });
     } else {
@@ -46,10 +46,10 @@ const scrollToRight = (slider) => {
       });
     }
   } else if (slider === "thirt-slider") {
-    if (slider3.scrollLeft === 1682) {
+    if (slider3.scrollLeft === 1664) {
       slider3.scrollBy({
         top: 0,
-        left: -1682,
+        left: -1664,
         behavior: "smooth",
       });
     } else {
@@ -84,28 +84,105 @@ const scrollToLeft = (slider) => {
   }
 };
 
-//VIDEO
+//VIDEO-----------------------------------------------------------
 
-let video = document.querySelector(".video");
-let playButton = document.querySelector("#play-btn");
-let soundButton = document.querySelector("#sound-btn");
+let interstallerVideo = document.querySelector("#interstallerVideo");
+let interstallerPlayButton = document.querySelector("#interstaller-play-btn");
+let interstallerSoundButton = document.querySelector("#interstaller-sound-btn");
+let interstallerDetail = document.querySelector("#interstaller-detail");
 
-const playVideo = () => {
-  if (video.paused) {
-    video.play();
-    playButton.className = "fa-solid fa-stop";
+let darkVideo = document.querySelector("#darkVideo");
+let darkPlayButton = document.querySelector("#dark-play-btn");
+let darkSoundButton = document.querySelector("#dark-sound-btn");
+let darkDetail = document.querySelector("#dark-detail");
+
+let djangoVideo = document.querySelector("#djangoVideo");
+let djangoPlayButton = document.querySelector("#django-play-btn");
+let djangoSoundButton = document.querySelector("#django-sound-btn");
+let djangoDetail = document.querySelector("#django-detail");
+
+const interstallerVideoPlay = () => {
+  if (interstallerVideo.paused) {
+    interstallerVideo.play();
+    interstallerPlayButton.className = "fa-solid fa-stop";
+    interstallerDetail.style.display = "none";
   } else {
-    video.pause();
-    playButton.className = "fa-solid fa-play";
+    interstallerVideo.pause();
+    interstallerPlayButton.className = "fa-solid fa-play";
+    interstallerDetail.style.display = "flex";
   }
 };
 
-const muteVideo = () => {
-  if (video.muted === false) {
-    video.muted = true;
-    soundButton.className = "fa-solid fa-volume-high";
-  } else if (video.muted === true) {
-    video.muted = false;
-    soundButton.className = "fa-solid fa-volume-xmark";
+const darkVideoPlay = () => {
+  if (darkVideo.paused) {
+    darkVideo.play();
+    darkPlayButton.className = "fa-solid fa-stop";
+    darkDetail.style.display = "none";
+  } else {
+    darkVideo.pause();
+    darkPlayButton.className = "fa-solid fa-play";
+    darkDetail.style.display = "flex";
+  }
+};
+
+const djangoVideoPlay = () => {
+  if (djangoVideo.paused) {
+    djangoVideo.play();
+    djangoPlayButton.className = "fa-solid fa-stop";
+    djangoDetail.style.display = "none";
+  } else {
+    djangoVideo.pause();
+    djangoPlayButton.className = "fa-solid fa-play";
+    djangoDetail.style.display = "flex";
+  }
+};
+
+const interstallerVideoMute = () => {
+  if (interstallerVideo.muted === false) {
+    interstallerVideo.muted = true;
+    interstallerSoundButton.className = "fa-solid fa-volume-high";
+  } else if (interstallerVideo.muted === true) {
+    interstallerVideo.muted = false;
+    interstallerSoundButton.className = "fa-solid fa-volume-xmark";
+  }
+};
+
+const darkVideoMute = () => {
+  if (darkVideo.muted === false) {
+    darkVideo.muted = true;
+    darkSoundButton.className = "fa-solid fa-volume-high";
+  } else if (darkVideo.muted === true) {
+    darkVideo.muted = false;
+    darkSoundButton.className = "fa-solid fa-volume-xmark";
+  }
+};
+
+const djangoVideoMute = () => {
+  if (djangoVideo.muted === false) {
+    djangoVideo.muted = true;
+    djangoSoundButton.className = "fa-solid fa-volume-high";
+  } else if (djangoVideo.muted === true) {
+    djangoVideo.muted = false;
+    djangoSoundButton.className = "fa-solid fa-volume-xmark";
+  }
+};
+
+const videoPlay = (video) => {
+  if (video === "interstaller") {
+    interstallerVideoPlay();
+  } else if (video === "dark") {
+    darkVideoPlay();
+  } else if (video === "django") {
+    djangoVideoPlay();
+  }
+};
+
+const videoMute = (video) => {
+  if (video === "interstaller") {
+    interstallerVideoMute();
+  } else if (video === "dark") {
+    darkVideoMute();
+  } else if (video === "django") {
+    djangoVideoMute();
   }
 };
