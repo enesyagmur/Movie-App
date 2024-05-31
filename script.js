a1a9a8ddd3ac346079db769fa4d080589f9b2e45
Enes Yağmur <enesyagmur99@gmail.com>
2024-06-01 01:01:32 +0300

    verileri api dan çekerek gösterimini yaptım

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const changeModeFunc = () => {
-  var button = document.getElementById("mode-button");
-
-  if (document.body.className) {
-    document.body.classList.remove("light");
-    button.classList.remove("left");
-  } else {
-    button.classList.add("left");
-
-    document.body.classList.add("light");
-  }
-};
-
 //SLIDER----------------------------------------------------------------
 let slider1 = document.querySelector("#first-slider");
 let slider2 = document.querySelector("#second-slider");
@@ -83,6 +70,66 @@ const scrollToLeft = (slider) => {
     });
   }
 };
+
+// GET
+const apiKey = "28e89d863e28cef1a0f8056f0c27867a";
+
+const documentaryUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=tr&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=99`;
+const populerSeriesUrl = `https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}&language=tr-TR&page=1`;
+const populerMoviessUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=tr&page=1`;
+
+fetch(populerMoviessUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.map((movie) => {
+      slider1.innerHTML += `<div class="slide" key=${movie.id}>
+      <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" alt="" />
+      <div class="slide-detail">
+        <p class="name">${movie.original_title}</p>
+        <p class="detail">
+         ${movie.overview}
+        </p>
+      </div>
+    </div>`;
+    });
+  })
+  .catch((err) => console.error(err));
+
+fetch(populerSeriesUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.map((serie) => {
+      slider2.innerHTML += `<div class="slide" key=${serie.id}>
+      <img src="https://image.tmdb.org/t/p/w500${serie.backdrop_path}" alt="" />
+      <div class="slide-detail">
+        <p class="name">${serie.original_name}</p>
+        <p class="detail">
+         ${serie.overview}
+        </p>
+      </div>
+    </div>`;
+    });
+  })
+  .catch((err) => console.error(err));
+
+fetch(documentaryUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.map((documentary) => {
+      if (documentary.overview) {
+        slider3.innerHTML += `<div class="slide" key=${documentary.id}>
+        <img src="https://image.tmdb.org/t/p/w500${documentary.backdrop_path}" alt="" />
+        <div class="slide-detail">
+          <p class="name">${documentary.title}</p>
+          <p class="detail">
+           ${documentary.overview}
+          </p>
+        </div>
+      </div>`;
+      }
+    });
+  })
+  .catch((err) => console.error(err));
 
 //VIDEO-----------------------------------------------------------
 
@@ -186,3 +233,7 @@ const videoMute = (video) => {
     djangoVideoMute();
   }
 };
+
+// 20 tl filmi çekebildim ama tam olarak istediğim veriyi çekebilmiş değilim
+// dark mode çalışmıyor
+// 3. videonun iconları değişmiyor
