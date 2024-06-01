1d26c03f4d319b6214d6cd1df93a5b387f157a7d
Enes Yağmur <enesyagmur99@gmail.com>
2024-06-01 10:51:16 +0300

    yine tekrar olması için html ye veri yazdırmanın farklı yöntemlerini kullandım

--- scripts/takeDataScripts.js
+++ scripts/takeDataScripts.js
@@ -13,15 +13,33 @@ fetch(populerMoviessUrl)
   .then((data) => {
     data.results.map((movie) => {
       if (movie.overview) {
-        movieSlide.innerHTML += `<div class="slide" key=${movie.id}>
-      <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" alt="" />
-      <div class="slide-detail">
-        <p class="name">${movie.title}</p>
-        <p class="detail">
-         ${movie.overview}
-        </p>
-      </div>
-    </div>`;
+        //tanımlamalar
+        const slide = document.createElement("div");
+        slide.className = "slide";
+        slide.setAttribute("key", movie.id);
+
+        const img = document.createElement("img");
+        img.src = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+        img.alt = movie.title;
+
+        const slideDetail = document.createElement("div");
+        slideDetail.className = "slide-detail";
+
+        const name = document.createElement("p");
+        name.className = "name";
+        name.textContent = movie.title;
+
+        const detail = document.createElement("p");
+        detail.className = "detail";
+        detail.textContent = movie.overview;
+
+        //soy ağacı
+
+        movieSlide.appendChild(slide);
+        slide.appendChild(img);
+        slide.append(slideDetail);
+        slideDetail.appendChild(name);
+        slideDetail.appendChild(detail);
       }
     });
   })
@@ -49,17 +67,34 @@ fetch(populerSeriesUrl)
 fetch(documentaryUrl)
   .then((response) => response.json())
   .then((data) => {
-    data.results.map((documentary) => {
+    data.results.forEach((documentary) => {
       if (documentary.overview) {
-        documentarySlide.innerHTML += `<div class="slide" key=${documentary.id}>
-        <img src="https://image.tmdb.org/t/p/w500${documentary.backdrop_path}" alt="" />
-        <div class="slide-detail">
-          <p class="name">${documentary.title}</p>
-          <p class="detail">
-           ${documentary.overview}
-          </p>
-        </div>
-      </div>`;
+        //tanımlamalar----
+        const slide = document.createElement("div");
+        slide.className = "slide";
+        slide.setAttribute("key", documentary.id);
+
+        const img = document.createElement("img");
+        img.src = `https://image.tmdb.org/t/p/w500${documentary.backdrop_path}`;
+        img.alt = documentary.title;
+
+        const slideDetail = document.createElement("div");
+        slideDetail.className = "slide-detail";
+
+        const name = document.createElement("p");
+        name.className = "name";
+        name.textContent = documentary.title;
+
+        const detail = document.createElement("p");
+        detail.className = "detail";
+        detail.textContent = documentary.overview;
+
+        //soy ağacı oluşturma :D
+        documentarySlide.appendChild(slide);
+        slide.appendChild(img);
+        slide.appendChild(slideDetail);
+        slideDetail.appendChild(name);
+        slideDetail.appendChild(detail);
       }
     });
   })
