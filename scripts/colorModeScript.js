03b8ad0b120430df5d802a211a9cdc219d1b3884
Enes Yağmur <enesyagmur99@gmail.com>
2024-06-02 11:38:52 +0300

    mode değişikliğini daha erişilebilir yapmak için değişken olarak tanımladım

--- scripts/colorModeScript.js
+++ scripts/colorModeScript.js
@@ -2,11 +2,28 @@ let body = document.body;
 let modeButton = document.querySelector("#mode-button");
 
 const modeChangeFunc = () => {
-  if (body.className) {
-    body.classList.remove("light");
-    modeButton.classList.remove("left");
-  } else {
-    body.classList.add("light");
-    modeButton.classList.add("left");
+  const root = document.documentElement;
+
+  const currentModeColor = getComputedStyle(root)
+    .getPropertyValue(`--bgColor`)
+    .trim();
+
+  let newBgColor;
+  let newFontColor;
+
+  if (currentModeColor === "#151515") {
+    newFontColor = "#151515";
+    newBgColor = "#d6d6d6";
+  } else if (currentModeColor === "#d6d6d6") {
+    newFontColor = "#d6d6d6";
+    newBgColor = "#151515";
+  }
+
+  if (newBgColor) {
+    root.style.setProperty(`--bgColor`, newBgColor);
+  }
+
+  if (newFontColor) {
+    root.style.setProperty(`--fontColor`, newFontColor);
   }
 };
